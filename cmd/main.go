2010b9f52b802ad58642bc42d20075014c89e651
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-16 19:59:05 +0300

    init server and database

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/ssergomol/Balance-Manager/pkg/apiserver"
+)
 
+func main() {
+	config := apiserver.NewConfig()
+	server := apiserver.CreateServer(config)
+
+	if err := server.Start(); err != nil {
+		logrus.Fatal(err)
+	}
 }
