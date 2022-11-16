6cad1d3a9272b5e98ce2d52c86245f97cbfd865d
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-17 00:11:40 +0300

    create tables

--- pkg/database/init_db.sql
+++ pkg/database/init_db.sql
@@ -1,2 +1,26 @@
 CREATE DATABASE balance_manager;
 
+CREATE TABLE balances(
+    id SERIAL PRIMARY KEY,
+    sum NUMERIC(18, 2)
+);
+
+CREATE TABLE accounts(
+    id SERIAL PRIMARY KEY,
+    sum NUMERIC(18, 2)
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    service_id INTEGER NOT NULL,
+    price NUMERIC(18, 2),
+    description VARCHAR(256),
+    execution_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    balance_id SERIAL UNIQUE REFERENCES balances(id),
+    account_id SERIAL REFERENCES accounts(id),
+    order_id SERIAL REFERENCES orders(id)
+);
