4c3a8d9b4eb4d06c25859b5e55d193c75e2ea6f4
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-16 13:45:39 +0300

    create server config

---
+++ pkg/apiserver/config.go
@@ -0,0 +1,14 @@
+package apiserver
+
+type ConfigServer struct {
+	BindAddress string
+	DatabaseURL string
+	LogLevel    string
+}
+
+func NewConfig() ConfigServer {
+	return ConfigServer{
+		BindAddress: ":8080",
+		LogLevel:    "debug",
+	}
+}
