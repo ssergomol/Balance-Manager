02347a309a2787cf453ae23fcbd7356957018219
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-16 22:58:39 +0300

    fix migrations

--- pkg/database/migrations/000001_create_users_table.up.sql
+++ pkg/database/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    balance_id SERIAL REFERENCES balances(id),
+    balance_id SERIAL UNIQUE REFERENCES balances(id),
     account_id SERIAL REFERENCES accounts(id),
     order_id SERIAL REFERENCES orders(id)
 );
--- pkg/database/migrations/000004_create_order_table.up.sql
+++ pkg/database/migrations/000004_create_order_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE orders(
     id SERIAL PRIMARY KEY,
-    service_id INTEGER,
+    service_id INTEGER NOT NULL,
     price NUMERIC(18, 2),
-    description TEXT(256)
+    description TEXT(256),
+    execution_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
