93792098bae4996aace1792107ff261778093d39
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-17 01:09:55 +0300

    fix models' types bug

--- pkg/models/account.go
+++ pkg/models/account.go
@@ -1,6 +1,6 @@
 package models
 
 type Account struct {
-	ID  uint        `json:"id"`
-	Sum interface{} `json:"sum"`
+	ID  uint   `json:"id"`
+	Sum string `json:"sum"`
 }
--- pkg/models/balance.go
+++ pkg/models/balance.go
@@ -1,6 +1,6 @@
 package models
 
 type Balance struct {
-	ID  uint        `json:"id"`
-	Sum interface{} `json:"sum"`
+	ID  uint   `json:"id"`
+	Sum string `json:"sum"`
 }
