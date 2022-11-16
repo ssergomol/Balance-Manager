a59ada3a38186951f17ab8dd9d46f454cefdcee0
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-16 18:16:38 +0300

    create server config

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -1,1 +1,15 @@
 package apiserver
+
+type APIserver struct {
+	config *ConfigServer
+}
+
+func CreateServer(config *ConfigServer) *APIserver {
+	return &APIserver{
+		config: config,
+	}
+}
+
+func (s *APIserver) Start() error {
+
+}
