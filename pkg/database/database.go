30418e7cc174c187f4394f1b8826e63ca639052c
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-17 17:51:44 +0300

    add access repo funcs

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Storage struct {
-	DB          *sql.DB
-	Config      *ConfigDB
+	db          *sql.DB
+	config      *ConfigDB
 	userRepo    *UserRepo
 	orderRepo   *OrderRepo
 	balanceRepo *BalanceRepo
@@ -17,12 +17,12 @@ type Storage struct {
 
 func NewDB(config *ConfigDB) *Storage {
 	return &Storage{
-		Config: config,
+		config: config,
 	}
 }
 
 func (s *Storage) Connect() error {
-	db, err := sql.Open("postgres", s.Config.databaseURL)
+	db, err := sql.Open("postgres", s.config.databaseURL)
 	if err != nil {
 		return err
 	}
@@ -32,10 +32,54 @@ func (s *Storage) Connect() error {
 		return err
 	}
 
-	s.DB = db
+	s.db = db
 	return nil
 }
 
 func (s *Storage) Disconnect() error {
-	return s.DB.Close()
+	return s.db.Close()
+}
+
+func (s *Storage) Order() *OrderRepo {
+	if s.orderRepo != nil {
+		return s.orderRepo
+	}
+
+	s.orderRepo = &OrderRepo{
+		store: s,
+	}
+	return s.orderRepo
+}
+
+func (s *Storage) User() *UserRepo {
+	if s.orderRepo != nil {
+		return s.userRepo
+	}
+
+	s.userRepo = &UserRepo{
+		store: s,
+	}
+	return s.userRepo
+}
+
+func (s *Storage) Balance() *BalanceRepo {
+	if s.balanceRepo != nil {
+		return s.balanceRepo
+	}
+
+	s.balanceRepo = &BalanceRepo{
+		store: s,
+	}
+	return s.balanceRepo
+}
+
+func (s *Storage) Account() *AccountRepo {
+	if s.accountRepo != nil {
+		return s.accountRepo
+	}
+
+	s.accountRepo = &AccountRepo{
+		store: s,
+	}
+	return s.accountRepo
 }
