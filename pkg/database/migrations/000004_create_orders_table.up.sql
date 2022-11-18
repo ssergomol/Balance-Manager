7ee52634ba5f58e861e5973e411cdc5f517f3c62
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-18 17:37:38 +0300

    fix migrations

--- pkg/database/migrations/000004_create_orders_table.up.sql
+++ pkg/database/migrations/000004_create_orders_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE orders(
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES orders(id),
     service_id INTEGER NOT NULL,
+    is_positive BOOLEAN NOT NULL,
     price NUMERIC(18, 2),
-    description VARCHAR(256),
     execution_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
