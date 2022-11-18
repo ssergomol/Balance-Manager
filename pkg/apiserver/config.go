309517c1c4d0c7259c8d91c0bc913fa4081cebc3
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-18 17:35:26 +0300

    complete config

--- pkg/apiserver/config.go
+++ pkg/apiserver/config.go
@@ -1,14 +1,26 @@
 package apiserver
 
+import (
+	"github.com/pelletier/go-toml"
+)
+
 type ConfigServer struct {
 	BindAddress string
 	DatabaseURL string
 	LogLevel    string
 }
 
-func NewConfig() *ConfigServer {
-	return &ConfigServer{
-		BindAddress: ":8080",
-		LogLevel:    "debug",
+func NewConfig() (*ConfigServer, error) {
+	config, err := toml.LoadFile("config.toml")
+	if err != nil {
+		return &ConfigServer{}, err
 	}
+
+	params := "host=" + config.Get("store.host").(string) + " dbname=" +
+		config.Get("store.dbname").(string) + " sslmode=" + config.Get("store.sslmode").(string)
+	return &ConfigServer{
+		BindAddress: config.Get("bind_addr").(string),
+		LogLevel:    config.Get("log_level").(string),
+		DatabaseURL: params,
+	}, nil
 }
--- pkg/database/config.go
+++ pkg/database/config.go
@@ -1,11 +1,31 @@
 package database
 
+import (
+	toml "github.com/pelletier/go-toml"
+)
+
 type ConfigDB struct {
-	databaseURL string
+	dbServer string
+	host     string
+	dbName   string
+	sslMode  string
+	dbURL    string
 }
 
-func NewConfig() *ConfigDB {
-	return &ConfigDB{
-		databaseURL: "host=localhost dbname=balance_manager sslmode=disable",
+func NewConfig() (*ConfigDB, error) {
+	config, err := toml.LoadFile("config.toml")
+	if err != nil {
+		return &ConfigDB{}, err
 	}
+
+	dbServer := "host=" + config.Get("store.host").(string) + " sslmode=" + config.Get("store.sslmode").(string)
+	fullURL := "host=" + config.Get("store.host").(string) + " dbname=" + config.Get("store.dbname").(string) + " sslmode=" + config.Get("store.sslmode").(string)
+
+	return &ConfigDB{
+		dbServer: dbServer,
+		host:     config.Get("store.host").(string),
+		dbName:   config.Get("store.dbname").(string),
+		sslMode:  config.Get("store.sslmode").(string),
+		dbURL:    fullURL,
+	}, nil
 }
