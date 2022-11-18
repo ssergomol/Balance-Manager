20fe60e5ffb80dd2d05a2ac3f10653ee74af350d
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-18 03:04:27 +0300

    add database fetch funcs

--- pkg/database/balance_repo.go
+++ pkg/database/balance_repo.go
@@ -1,6 +1,11 @@
 package database
 
-import "github.com/ssergomol/Balance-Manager/pkg/models"
+import (
+	"database/sql"
+
+	"github.com/shopspring/decimal"
+	"github.com/ssergomol/Balance-Manager/pkg/models"
+)
 
 type BalanceRepo struct {
 	store *Storage
@@ -11,4 +16,60 @@ func (r *BalanceRepo) CreateBalance(balance models.Balance) {
 		`INSERT INTO balances (sum) VALUES ($1)`,
 		balance.Sum,
 	)
+}
+
+func (r *BalanceRepo) ReplenishBalance(balance models.Balance) error {
+	var oldSum string
+	if err := r.store.db.QueryRow("SELECT sum from balances WHERE user_id = $1", balance.ID).Scan(&oldSum); err != nil {
+		if err == sql.ErrNoRows {
+			r.store.db.QueryRow(
+				`INSERT INTO balances (user_id, sum) VALUES ($1, $2)`,
+				balance.ID, balance.Sum,
+			)
+			return nil
+		}
+		return err
+	}
+	err := r.store.db.QueryRow("SELECT sum from balances WHERE user_id = $1", balance.ID).Scan(&oldSum)
+	if err != nil {
+		return err
+	}
+
+	sum, err := decimal.NewFromString(oldSum)
+	if err != nil {
+		return err
+	}
+
+	deltaSum, err := decimal.NewFromString(balance.Sum)
+	if err != nil {
+		return err
+	}
+	newSum := sum.Add(deltaSum)
+
+	r.store.db.QueryRow("UPDATE balances SET sum = $1 WHERE user_id = $2", newSum.String(), balance.ID)
+	return nil
+}
+
+func (r *BalanceRepo) GetBalance(userID uint) (models.Balance, error) {
+	var sum string
+	if err := r.store.db.QueryRow("SELECT sum from balances WHERE user_id = $1", userID).Scan(&sum); err != nil {
+		if err == sql.ErrNoRows {
+			r.store.db.QueryRow(
+				`INSERT INTO balances (user_id, sum) VALUES ($1, $2)`,
+				userID, "0.00",
+			)
+			balance := models.Balance{
+				ID:  userID,
+				Sum: "0.00",
+			}
+			return balance, nil
+		}
+		return models.Balance{}, err
+	}
+
+	balance := models.Balance{
+		ID:  userID,
+		Sum: sum,
+	}
+	return balance, nil
 }
