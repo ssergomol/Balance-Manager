29b02720e7246ce164da83ab7f300025fbcb1967
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-18 14:15:51 +0300

    add more fields

--- pkg/models/account.go
+++ pkg/models/account.go
@@ -1,6 +1,8 @@
 package models
 
 type Account struct {
-	ID  uint   `json:"id"`
-	Sum string `json:"sum"`
+	ID        uint   `json:"id"`
+	UserID    uint   `json:"user_id"`
+	ServiceID uint   `json:"service_id"`
+	Sum       string `json:"sum"`
 }
